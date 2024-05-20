32d45e3f1724c60e76984e6f7bbb37107822ce21
Tyler Reed <136347704+treed-eaglemetal@users.noreply.github.com>
2024-05-20 16:24:38 -0500

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\PortalUser;
-use Illuminate\Routing\Controller;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class UserController extends Controller
 {
